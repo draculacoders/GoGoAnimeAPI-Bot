ed4704b47a4de215886066388883433e2fed4c22
Dracula Coders <112293593+draculacoders@users.noreply.github.com>
2023-01-29 17:49:37 +0530

    Update helper.py

--- Anime_Downloader_Probot/helper.py
+++ Anime_Downloader_Probot/helper.py
@@ -11,4 +11,4 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules[f"Anime_Downloader_Probot.plugins.{plugin_name}"] = load
-    print(f"Anime_Downloader_Probot Import{plugin_name}")
+    print(f"xAnimeDLBot Import{plugin_name}")
